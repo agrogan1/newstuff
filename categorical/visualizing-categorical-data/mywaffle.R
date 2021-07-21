39adb3209fd963658651a9aa05cde31e0da228e4
Andrew Grogan-Kaylor <agrogan@Andrews-MacBook-Air.local>
2021-07-21 11:56:21 -0400

    re-render w/ alluvial diagrams!!!

--- categorical/visualizing-categorical-data/mywaffle.R
+++ categorical/visualizing-categorical-data/mywaffle.R
@@ -7,7 +7,7 @@ names <- c("Placebo; No Cold",
            "Ascorbic Acid; No Cold",
            "Ascorbic Acid; Cold")
 
-vals <- c(109,31,122,17)
+vals <- c(109, 31, 122, 17)
 
 parts <- data.frame(names, vals)
 
